80e6b9a3f85abfd7b0b73742210ed7f503ce3ebc
marina <you@example.com>
2022-08-17 14:49:11 +0300

    first commit

---
+++ clock.js
@@ -0,0 +1,9 @@
+// let clock = document.getElementById('clock');
+// let body = document.getElementById('body');
+
+// function time(){
+    // const d = new Date();
+    // document.getElementById('clock').innerHTML = d;
+
+// }
+
